fd5c7bcdb4e474b45dfb2ae5f7011ff6eb46a06a
projectmandiri10-lang <projectmandiri10@gmail.com>
2026-05-07 11:22:36 +0700

    Open Chrome when starting dev server

--- start-dev.bat
+++ start-dev.bat
@@ -3,6 +3,7 @@ setlocal
 
 cd /d "%~dp0"
 set "NEED_REPAIR=0"
+set "DEV_URL=http://localhost:5173"
 
 if not exist "node_modules" (
   echo [INFO] Installing dependencies...
@@ -30,4 +31,6 @@ if "%NEED_REPAIR%"=="1" (
 )
 
 echo [INFO] Starting server + frontend (dev mode)...
+echo [INFO] Opening Chrome at %DEV_URL% shortly...
+start "" powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "Start-Sleep -Seconds 3; $url = '%DEV_URL%'; $paths = @(($env:ProgramFiles + '\Google\Chrome\Application\chrome.exe'), (${env:ProgramFiles(x86)} + '\Google\Chrome\Application\chrome.exe'), ($env:LocalAppData + '\Google\Chrome\Application\chrome.exe')); $chrome = $paths | Where-Object { $_ -and (Test-Path $_) } | Select-Object -First 1; if ($chrome) { Start-Process -FilePath $chrome -ArgumentList $url } else { try { Start-Process -FilePath 'chrome.exe' -ArgumentList $url } catch { Start-Process -FilePath $url } }"
 call npm run dev
